93420908a05e97b7abb7ca865de428f5457c00bb
San096 <sanderleycc2022@alu.ufc.br>
2026-01-01 20:38:24 -0300

    finalização

--- BackEnd/.venv/Scripts/activate.bat
+++ BackEnd/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\sande\Desktop\Trabalho final de web\Guia-Sa-de-\BackEnd\.venv"
+set "VIRTUAL_ENV=C:\Users\sande\Desktop\Trabalho final de web\Guia-Sa-de_BackEnd\BackEnd\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
